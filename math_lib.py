c972faa5ce300094b7e7e083ffcdc300851c01bc
Jack Kawell <jgkawell@outlook.com>
2019-09-03 11:13:59 -0600

    Added documentation to math_lib

--- math_lib.py
+++ math_lib.py
@@ -1,4 +1,17 @@
 def div(a, b):
+    """
+    Divides two inputs (checks for division by zero) 
+    
+    Parameters
+    ----------
+    a (double): The numerator for the division
+    b (double): The denominator for the division
+
+    Returns
+    ----------
+    d (double): The result of the division
+    
+    """
     if b == 0:
         # cannot divide by zero
         return None
@@ -7,4 +20,17 @@ def div(a, b):
 
 
 def add(a, b):
+    """
+    Adds two numeric inputs
+    
+    Parameters
+    ----------
+    a (double): The first number to sum
+    b (double): The second number to sum
+
+    Returns
+    ----------
+    d (double): The result of the summation
+    
+    """
     return a + b
